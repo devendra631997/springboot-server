db41838295e34e14385cfd62c0ddd8670e151ce9
devendra <you@example.com>
2021-09-02 19:43:27 +0530

    first one

--- src/main/java/com/example/herokudemo/IndexController.java
+++ src/main/java/com/example/herokudemo/IndexController.java
@@ -8,6 +8,6 @@ public class IndexController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello there! I'm running.";
+        return "Dont Worry! I'm running.  🌋 🚒  🔥 ";
     }
 }
